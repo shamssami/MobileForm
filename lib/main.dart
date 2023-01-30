281497c177ad73be1f8b5ab3afb03a600c232e96
shamssami <shamsalman1290@gmail.com>
2023-01-30 22:33:20 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FreeCode Spot',
+      title: 'FLutter Form',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
